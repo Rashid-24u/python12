5dc7d37aef05be4f69b101f94d15727dbd19d89b
Rashid <rashidridhu10@gmail.com>
2025-06-09 21:13:28 +0530

    third commit

---
+++ class14.py
@@ -0,0 +1,8 @@
+def main():
+    library = Library()
+    while True:
+        print("\n=== Main Menu ===")
+        print("1. Admin")
+        print("2. User")
+        print("3. Exit")
+        choice = input("Choose option: ")
