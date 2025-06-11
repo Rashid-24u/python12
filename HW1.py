55fda60c80f1e2f47388e55d613d909aa993240d
Rashid <rashidridhu10@gmail.com>
2025-06-11 20:37:38 +0530

    new

---
+++ HW1.py
@@ -0,0 +1,104 @@
+# 1. With argument, with return type
+# Q1. Write a function that takes a number as an argument and returns its square.
+
+# def square(num):
+#     return num ** 2
+# result=square(int(input("Enter the no:")))
+# print("square:",result)
+
+# Q2. Write a function that takes two numbers and returns their sum.
+
+# def SUM(n1,n2):
+#     return(n1+n2)
+
+# a=int(input("Enter the 1st number:"))
+# b=int(input("Enter the 2nd number:"))
+# print(SUM(a,b))
+
+
+# Q3. Write a function that takes a string and returns it in uppercase.
+
+# def uppercase(text):
+#     txt=text.upper()
+#     return txt
+# a=input("enter a word :")
+# print(uppercase(a))
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..
+
+# 2. With argument, without return type
+# Q4. Write a function that takes a name as an argument and prints a greeting message
+
+# def greet(name):
+#     print("hello",name+"!")
+# greet("Ridhu") 
+
+
+# Q5. Write a function that takes two numbers and prints their multiplication.
+
+# def multiplication(a,b):
+#     result=a*b
+#     print("Product is",result)
+# a=int(input("Enter 1st no :"))
+# b=int(input("Enter 2nd no :"))
+# multiplication(a,b)    
+
+# Q6. Write a function that takes a number and prints whether it is even or odd.
+
+# def Even_Odd(num):
+#     if num % 2 ==0:
+#         print(num,"is Even")
+#     else:
+#         print(num,"is Odd") 
+# a=int(input("Enter a number..:"))
+# Even_Odd(a) 
+# 
+
+ #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>....
+ # 
+ #  3. Without argument, with return type
+# Q7. Write a function that returns the current year.
+
+# def current_year():
+#     year =2025
+#     return year
+# yr=current_year()
+# print("The current year is:", yr)
+
+
+
+# Q8. Write a function that returns a default welcome message.
+
+# def wel_msg():
+#     message="Welcome to Python"
+#     return message
+# msg =wel_msg()
+# print(msg)
+
+# Q9. Write a function that returns the value of 5 factorial (5!).
+   
+# def factorial():
+#     factorial = 1
+#     for i in range(1,6):
+#         factorial *= i
+#     return factorial
+# result = factorial()
+# print("5! =", result)
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.....................
+
+
+
+# Without argument, without return type
+# Q10. Write a function that prints your name.
+
+# def print_name():
+#     print("Rashid")
+# print_name()
+
+# Q11. Write a function that prints numbers from 1 to 10.
+
+# def number():
+#     for i in range(1,11):
+#         print(i)
+# number()        
---
+++ class16.py
@@ -0,0 +1,107 @@
+# def square(num):
+#     return num**2
+# object_=square(6)
+# print("the square of the given no:",object_)
+
+
+
+
+
+# # Define a function to calculate sum of two numbers
+# def add(a, b):
+#     return a + b
+# # Call the function
+# sum_result = add(10, 20)
+
+# print(f"The sum is {sum_result}")
+
+
+
+
+# def square(num):
+#     return num**2
+# object_=square(int(input("enter the no:")))
+# print("the square of the given no:",object_)
+
+# ...................or.......................
+
+# def square(num):
+#     return num**2
+# object_=(int(input("enter the no:")))
+# result=square(object_)
+# print("the square of the given no:",result)
+
+# ..................or...................
+
+# def square(num):
+#     return num**2
+# object_=(int(input("enter the no:")))
+# print("the square of the given no:",square(object_))
+
+
+# def a_fn(string):
+#     "this prints the value of length of string"
+#     return len (string)
+
+# print(a_fn("Functions"))
+# print(a_fn("python"))
+
+
+
+
+
+
+
+
+# def square(num,num1):
+#     return(num**num1)
+
+# # def square(num,num1):      ..........................ith workaavollu
+#     return(num+num1)
+
+# a=int(input("Enter the number:"))
+# b=int(input("Enter the power:"))
+# print(square(a,b))
+# >..........................................................................................................
+
+
+
+# with argument with return type
+
+
+# fn to calculate square of a no.
+
+# def square(num):
+#     return num * num
+# result=square(5)
+# print("square:",result)
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+# with argument without return type
+# fn to print greeting msg
+
+# def greet(name):
+#     print("helo",name+"!")
+# greet("momi")    
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+# without argument with return type
+
+# fn that returns a wlcm msg
+
+# def get_message():
+#     return "Welcm to python prgrmming"
+# msg=get_message()
+# print(msg)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# without argument without return type
+
+# fn to print no.s from 1 to 5
+def print_no():
+    for i in range(1,6):
+        print(i)
+print_no()        
